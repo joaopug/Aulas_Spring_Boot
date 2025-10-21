2b14ba15bc13603cd3776ac40217f141322204fe
joaopug <41847866+joaopug@users.noreply.github.com>
2025-10-21 20:08:07 -0300

    Agora dá pra editar

--- src/main/kotlin/br/com/unipar/exemplo/controller/PessoaController.kt
+++ src/main/kotlin/br/com/unipar/exemplo/controller/PessoaController.kt
@@ -4,18 +4,22 @@ import br.com.unipar.exemplo.database.PessoasRepository
 import br.com.unipar.exemplo.model.Pessoa
 import org.apache.coyote.Response
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.String
 
 //
 
 //A anotação RestController não pode usar VIEW, basicamente é para contruir APIs
 //A anotação Controller permite ter VIEW, para construir Web Applications
+@CrossOrigin(origins = ["http://localhost:5173"])
 
 //Endpoint é uma rota onde você
 @RestController
@@ -48,21 +52,36 @@ class PessoaController(
 
     @GetMapping("/{id}")
     fun listarId(@PathVariable id: Long): ResponseEntity<Pessoa> {
-        val pessoa = pessoaRepository.findById(id).get()
-        return if (pessoa != null) {
-            ResponseEntity.ok(pessoa)
+        val pessoa = pessoaRepository.findById(id)
+        return if (pessoa.isPresent) {
+            ResponseEntity.ok(pessoa.get())
         } else {
             ResponseEntity.notFound().build()
         }
     }
 
     @DeleteMapping("/{id}")
-    fun excluirPessoa (@PathVariable id: Long): ResponseEntity<Void> {
+    fun excluirPessoa(@PathVariable id: Long): ResponseEntity<Void> {
         val pessoa = pessoaRepository.deleteById(id)
-        return if (pessoa != null) {
+        return if (pessoaRepository.existsById(id)) {
             ResponseEntity.noContent().build()
         } else {
             ResponseEntity.notFound().build()
         }
+    }
+
+    @PutMapping("/{id}")
+    fun atualizarPessoa(@PathVariable id: Long, @RequestBody novaPessoa: Pessoa)
+            : ResponseEntity<Pessoa> {
+        return pessoaRepository.findById(id).map { pessoa ->
+            val pessoaAtualizada = pessoa.copy(
+                nome = novaPessoa.nome,
+                idade = novaPessoa.idade,
+                cpf = novaPessoa.cpf,
+            )
+            ResponseEntity.ok(pessoaRepository.save(pessoaAtualizada))
+        }.orElse(
+            ResponseEntity.notFound().build()
+        )
     }
 }
